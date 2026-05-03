abad36209cfaed131c773f27f3cbb68281ea53d8
Gabriele Fiorucci <gabrielefiorucci03@gmail.com>
2026-05-03 11:21:20 +0200

    ci: add jest setup for exam-planning, untrack database.module from gitignore, fix degree-courses spec providers

--- libs/server/exam-planning/src/lib/degree-courses/degree-courses.controller.spec.ts
+++ libs/server/exam-planning/src/lib/degree-courses/degree-courses.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DegreeCoursesController } from './degree-courses.controller';
+import { DegreeCoursesService } from './degree-courses.service';
 
 describe('DegreeCoursesController', () => {
   let controller: DegreeCoursesController;
@@ -7,6 +8,7 @@ describe('DegreeCoursesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DegreeCoursesController],
+      providers: [{ provide: DegreeCoursesService, useValue: {} }],
     }).compile();
 
     controller = module.get<DegreeCoursesController>(DegreeCoursesController);
--- libs/server/exam-planning/src/lib/degree-courses/degree-courses.service.spec.ts
+++ libs/server/exam-planning/src/lib/degree-courses/degree-courses.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DegreeCoursesService } from './degree-courses.service';
+import { DegreeCoursesRepository } from './degree-courses.repository';
 
 describe('DegreeCoursesService', () => {
   let service: DegreeCoursesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DegreeCoursesService],
+      providers: [
+        DegreeCoursesService,
+        { provide: DegreeCoursesRepository, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<DegreeCoursesService>(DegreeCoursesService);
